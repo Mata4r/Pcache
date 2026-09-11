09921cab125350637f97b94ba2f788672b8204c9
Matar <143889648+Mata4r@users.noreply.github.com>
2026-09-11 14:47:04 +0300

    Refactor documentation and improve error messages

--- modules/pcache_passive_scan.py
+++ modules/pcache_passive_scan.py
@@ -50,7 +50,7 @@ try:
         config = json.load(f)
 except FileNotFoundError as e:
     print(
-        f"Configuration file not found {e}"
+        f"Configuration file not found: {e}"
     )
     sys.exit(1)
 
